ade1d0fd14a6585cabadf2d1d5fb133500ccc903
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-02-14 13:04:01 +0000

    Update twrp_a12s.mk

--- twrp_a12s.mk
+++ twrp_a12s.mk
@@ -4,12 +4,15 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+DEVICE_PATH := device/samsung/a12s
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Release name
+PRODUCT_RELEASE_NAME := a12s
 
-# Inherit some common twrp stuff.
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a12s device
